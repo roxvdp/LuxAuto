e9efb0d8eaa876cb5ff8622272d07eafe98bcf51
roxvdp <roxanne.vandputten@gmail.com>
2025-06-16 20:51:39 +0200

    API in database geïntegreerd

--- app/database/__init__.py
+++ app/database/__init__.py
@@ -1,22 +1,5 @@
-# app/database/__init__.py
+from .session import SessionLocal
 
 from flask_sqlalchemy import SQLAlchemy
-from os import getenv
-from dotenv import load_dotenv
-
-# .env inladen (voor zowel Flask als Alembic)
-load_dotenv()
-
 # Initialiseer SQLAlchemy object
 db = SQLAlchemy()
-
-def init_app(app):
-    # Stel DB-URI in als deze nog niet in app.config zit
-    if "SQLALCHEMY_DATABASE_URI" not in app.config:
-        app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
-
-    # Optioneel: geen waarschuwingen
-    app.config.setdefault("SQLALCHEMY_TRACK_MODIFICATIONS", False)
-
-    # Koppel db aan Flask-app
-    db.init_app(app)
